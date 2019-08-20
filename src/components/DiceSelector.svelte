8fba170c75e2de34f99758ec65e7024b51af4330
TJ Cole <gamester4433@gmail.com>
2019-08-20 00:33:04 -0700

    Update page title. Disallow text selection on dice buttons

--- src/components/DiceSelector.svelte
+++ src/components/DiceSelector.svelte
@@ -47,6 +47,13 @@
     line-height: 100%;
     cursor: pointer;
     margin-bottom: 10px;
+
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none;
   }
 
 
